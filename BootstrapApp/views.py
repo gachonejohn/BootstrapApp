1547e70a9579eae8e3fcc7cd10e0419a8d0c9d75
John Thuku <ceogachone@gmail.com>
2024-11-11 11:34:32 +0300

    BootstrapApp

---
+++ BootstrapApp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'index.html')
+    
+def about(request):
+    return render(request, 'about-us.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def services(request):
+    return render(request, 'services.html')
